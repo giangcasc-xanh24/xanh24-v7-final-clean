2240519357f14de5a7c018be23ff2823efb358c8
giangcasc-xanh24 <giang.casc@gmail.com>
2026-09-12 17:18:18 +0700

    Update sw.js
    Fix PWA sw.js unregister Day5

--- public/sw.js
+++ public/sw.js
@@ -2,3 +2,7 @@
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys();for(const k of ks) await caches.delete(k); await self.registration.unregister();})())});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request))});
+// Day8 FINAL + Phase1 Go Live - Fix PWA Day5 - Unregister Day5 SW - Full 12 Routes Stable - CHECKIN_1789105301946
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys(); for(const k of ks) await caches.delete(k); await self.registration.unregister();})())});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request))});
